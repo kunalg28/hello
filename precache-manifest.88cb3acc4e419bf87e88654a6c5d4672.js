7514046c3c4c02280360615ece1433b6cfda3f30
Kunal Gurjar <you@example.com>
2020-08-10 10:44:21 +0530

    Updates

---
+++ precache-manifest.88cb3acc4e419bf87e88654a6c5d4672.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d27472ac120c3f1da4cd603209365b",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "bb01ca587daee681c715",
+    "url": "/hello/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2c265c2a49f65f8f9ce7",
+    "url": "/hello/static/css/main.cb83475f.chunk.css"
+  },
+  {
+    "revision": "bb01ca587daee681c715",
+    "url": "/hello/static/js/2.5fdced64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello/static/js/2.5fdced64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c265c2a49f65f8f9ce7",
+    "url": "/hello/static/js/main.9c53363d.chunk.js"
+  },
+  {
+    "revision": "9e8a1ff2d898550a38b6",
+    "url": "/hello/static/js/runtime-main.b7b055bc.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/hello/static/media/SEGA.12a3db8b.woff"
+  }
+]);
